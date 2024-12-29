b3cc833afa6792e410309abd46e235628f07b16a
MuraKon2906 <mcray2906writes@gmail.com>
2024-12-29 20:10:51 +0000

    lol

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -12,79 +12,32 @@ return {
     config = function()
       require("mason-lspconfig").setup({
         ensure_installed = {
-          "lua_ls",
-          "pyright",
-          "rust_analyzer",
           "ts_ls",
           "jsonls",
           "bashls",
           "yamlls",
-          "ast_grep",
-          --"solargraph",
-          "tailwindcss",
+          "lua_ls",
           "html",
           "clangd",
+          "pyright",
         },
       })
     end,
   },
   {
     "neovim/nvim-lspconfig",
+    dependencies = { "saghen/blink.cmp" },
     lazy = false,
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
-      -- Ensure clangd uses proper offsetEncoding
-      capabilities.offsetEncoding = { "utf-16" }
-
+      local capabilities = require("blink.cmp").get_lsp_capabilities()
       local lspconfig = require("lspconfig")
-      lspconfig.tailwindcss.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
 
-      lspconfig.pyright.setup({
+      lspconfig["lua-ls"].setup({
         capabilities = capabilities,
       })
-
-      lspconfig.jsonls.setup({
+      lspconfig["pyright"].setup({
         capabilities = capabilities,
       })
-      lspconfig.bashls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.yamlls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-
-      -- lspconfig.rust_analyzer.setup({ -- for rust
-      --   capabilities = capabilities,
-      -- })
-      -- Clangd-specific setup
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        cmd = { "clangd", "--offset-encoding=utf-16" }, -- Ensure offset encoding
-        filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
-        root_dir = lspconfig.util.root_pattern("compile_commands.json", ".git"),
-      })
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
